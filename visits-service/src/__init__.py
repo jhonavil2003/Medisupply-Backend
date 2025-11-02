dc1a8125adcd7a117eda186f9f8ce8d3df33a8b4
dannamilenacoral <d.corall@uniandes.edu.co>
2025-11-01 23:30:26 -0500

    Ajuste carpetas

--- visits-service/src/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Servicio de Visitas - Medisupply Backend
-
-Este servicio maneja la funcionalidad relacionada con visitas a clientes,
-incluyendo la gestión de vendedores, programación de visitas y archivos adjuntos.
-"""
--- visits-service/src/blueprints/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Blueprints para endpoints del servicio de visitas
-"""
-
-from .visits import visits_bp
-
-__all__ = ['visits_bp']
